3f60df26bbb054d63d50dfbd71c19c5d8a0eb721
Maksym Rutkovskyi <maksym.rutkovskyi@Maksyms-Laptop.local>
2024-08-29 08:11:42 +0300

    styles

--- reports.js
+++ reports.js
@@ -1,14 +1,18 @@
 export default () => ({
     reportComponent: {
         report: JSON.parse(localStorage.getItem('report')) || [],
+        notify: {
+            show: false,
+            message: ''
+        },
         addProject: function(title) {
             this.report.push({title: title, tasks: []});
         },
-        deleteProject: function(index) {
+        deleteProject: function (index) {
             this.report.splice(index, 1);
             this.storeReport();
         },
-        addTask: function(projectIndex, title, hours) {
+        addTask: function (projectIndex, title, hours) {
             this.report[projectIndex].tasks.push({title: title, hours: hours});
             this.storeReport();
         },
@@ -25,8 +29,29 @@ export default () => ({
                 return acc + projectIndex + ') ' + project.title + '\n' + project.tasks.reduce((acc, task, index) => {
                     const taskIndex = index + 1;
                     totalHours += parseInt(task.hours);
-                    return acc + '  ' + projectIndex + '.' + taskIndex + ') ' + task.title + ' - ' + task.hours + ' години\n';
+                    return acc + '  ' + projectIndex + '.' + taskIndex + ') ' + task.title + ' - ' + task.hours + this.getHoursEnding(task.hours) + '\n';
                 }, '')
-        }, '') + 'Сумарно пропрацював ' + totalHours + ' годин';}
+            }, '') + 'Сумарно пропрацював ' + totalHours + this.getHoursEnding(totalHours);
+        },
+        copyToClipboard: function () {
+            return navigator.clipboard.writeText(this.stringifyReport())
+                .then(this.showNotify({show: true, message: 'Скопійовано в буфер обміну'}))
+                .catch();
+        },
+        showNotify: function (notification) {
+            this.notify = notification;
+            setTimeout(() => {
+                this.notify = {show: false, message: ''};
+            }, 1500);
+        },
+        getHoursEnding: function (hours) {
+            if (hours === 1) {
+                return ' годину';
+            } else if (hours >= 2 && hours <= 4) {
+                return ' години';
+            } else {
+                return ' годин';
+            }
+        }
     },
 })
